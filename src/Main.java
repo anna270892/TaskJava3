df6a0d06b79d0596d170be97bacd14f5cc86e42b
Anna Kochergina <akochergina92@gmail.com>
2023-08-23 20:26:49 +0300

    optimization

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         int check = 100; //текущий счет
-        int refill = 1500;  //пополнение счета
+        int refill = 1000;  //пополнение счета
         int bonus = 0; //бонусы при полнении от 1000 руб.
 
         if (refill <= 1000) {
             check = check + refill;
             System.out.println("Ваш текущий счет: " + check + " руб.");
             System.out.println("Бонусные рубли: " + bonus + " руб.");
-        }
-
-        if (refill > 1000) {
+        } else if (refill > 1000) {
             bonus = (refill / 100);
             check = (check + refill + bonus);
             System.out.println("Ваш текущий счет: " + check + " руб.");
             System.out.println("Бонусные рубли: " + bonus + " руб.");
         }
+
     }
 }
